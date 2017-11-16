2580983cd59a9e4b23408affdd619e79e52db3af
feizhihui <helloworld@csu.edu.cn>
2017-11-16 09:09:50 +0800

    add Deep Q Network

---
+++ DeepQ-Network/run_this.py
@@ -0,0 +1,54 @@
+from maze_env import Maze
+from RL_brain import DeepQNetwork
+
+
+def run_maze():
+    step = 0
+    # set total game episode number
+    for episode in range(300):
+        # initial observation
+        observation = env.reset()
+
+        while True:
+            # fresh env after each step
+            env.render()
+
+            # RL choose action based on observation
+            action = RL.choose_action(observation)
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            RL.store_transition(observation, action, reward, observation_)
+
+            if (step > 200) and (step % 5 == 0):
+                RL.learn()
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
+            step += 1
+
+    # end of game
+    print('game over')
+    env.destroy()
+
+
+if __name__ == "__main__":
+    # maze game
+    env = Maze()
+    print("actions:", env.n_actions, "features:", env.n_features)
+    RL = DeepQNetwork(env.n_actions, env.n_features,
+                      learning_rate=0.01,
+                      reward_decay=0.9,
+                      e_greedy=0.9,
+                      replace_target_iter=200,
+                      memory_size=2000,
+                      # output_graph=True
+                      )
+    env.after(100, run_maze)
+    env.mainloop()
+    RL.plot_cost()
